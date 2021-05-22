a711acda25c9af70378913003efb4341ffb6b0e5
Guilherme Calassi <guilherme@yourmove.com.br>
2021-05-22 16:02:49 +0100

    int 10

---
+++ queries/int10.sql
@@ -0,0 +1,9 @@
+.mode columns
+.headers ON
+.nullvalue NULL
+
+-- Utentes que não foram contactos para receberem nenhuma vacina
+SELECT Utente.* FROM Utente
+LEFT JOIN Contacto ON Contacto.utenteID = Utente.id
+WHERE Contacto.utenteID IS NULL
+ORDER BY Utente.prioridade DESC, Utente.dataNascimento ASC
