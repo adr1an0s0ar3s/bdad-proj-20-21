e5598caa9123821d000981648e2cebf053f4df76
Guilherme Calassi <guilherme@yourmove.com.br>
2021-05-22 15:38:59 +0100

    format int3

--- queries/int3.sql
+++ queries/int3.sql
@@ -1,6 +1,12 @@
+.mode columns
+.headers ON
+.nullvalue NULL
+
+-- Vacinas vencidas de um determinado centro: Data de Entrega + Tempo de Conversação não superior ao dia de hoje
 SELECT Vacina.*, Centro.email, JulianDay(Estado.dataAtualizacao) from Vacina, Encomenda, Estado, Centro
-WHERE Estado.estado = 'entregue'
-AND Encomenda.id = Estado.encomendaID
-AND Centro.id = Encomenda.centroID
-AND Vacina.id = Encomenda.vacinaID 
-AND ((JulianDay(Estado.dataAtualizacao) * 24) + JulianDay(Vacina.tempoConservacao)) < ((JulianDay('now') * 24))
+    WHERE 
+        Estado.estado = 'entregue'
+        AND Encomenda.id = Estado.encomendaID
+        AND Centro.id = Encomenda.centroID
+        AND Vacina.id = Encomenda.vacinaID 
+        AND ((JulianDay(Estado.dataAtualizacao) * 24) + JulianDay(Vacina.tempoConservacao)) < ((JulianDay('now') * 24))
