f0ab34dd58b08cfdfeb45a282161fbe632df6382
Guilherme Calassi <guilherme@yourmove.com.br>
2021-05-22 15:51:40 +0100

    int5

---
+++ queries/int5.sql
@@ -0,0 +1,11 @@
+.mode columns
+.headers ON
+.nullvalue NULL
+
+-- Contactos das vacinas já aplicadas por centro a um utente.
+SELECT Contacto.*, Utente.nome, Vacina.nome, Centro.email FROM Contacto, Utente, Vacina, Centro
+    WHERE  
+        JulianDay(Contacto.dataComparecimento) < JulianDay('now')
+        AND Utente.id = Contacto.utenteID
+        AND Vacina.id = Contacto.vacinaID
+        AND Centro.id = Utente.centroID
